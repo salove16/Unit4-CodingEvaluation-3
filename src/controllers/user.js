b5f5a279f586b887dd9898c51d7de2c87e3c33ad
salove16 <shashank.shalabh909@gmail.com>
2022-03-21 10:54:30 +0530

    Create user.js

---
+++ src/controllers/user.js
@@ -0,0 +1,13 @@
+const express=require("express")
+const router=express.Router()
+
+
+
+router.get("/",async(req,res)=>{
+    try {
+        const page=req.query.page
+        const pageSize
+    } catch (err) {
+        
+    }
+})
